518d78bc3831fbc6f4a0d3037bb47dae28b84296
Tushar shivade <tusharshivade122@gmail.com>
2026-03-08 17:59:46 +0530

    added test

--- demo.py
+++
@@ -1,7 +0,0 @@
-def my_function():
-    a=5
-    b=6
-
-    return d
-
-my_function()
---
+++ demo1.py
@@ -0,0 +1,8 @@
+def check_even_odd(num):
+    if num % 2 == 0:
+        print("Even number")
+    else:
+        print("Odd number")
+
+number = int(input("Enter a number: "))
+check_even_odd(number)
