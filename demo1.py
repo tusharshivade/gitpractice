9bd6cfb2061ae38062216810f803934b1f3f82db
Tushar shivade <tusharshivade122@gmail.com>
2026-03-08 18:30:27 +0530

    add  now

--- demo1.py
+++ demo1.py
@@ -1,9 +1,13 @@
+"""Simple program to check whether a number is even or odd."""
+
+
 def check_even_odd(num):
+    """Check if the given number is even or odd."""
     if num % 2 == 0:
         print("Even number")
     else:
         print("Odd number")
 
-        
+
 number = int(input("Enter a number: "))
 check_even_odd(number)
